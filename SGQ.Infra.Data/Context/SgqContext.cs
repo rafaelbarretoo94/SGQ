c22dbb677a061758b4ff92e71a8a731933d286ab
Rafael Barreto <rafaelbarretoo2008@hotmail.com>
2019-08-17 18:34:52 -0300

    BL_DEV: Configuração de banco de dados e inclusão de arquivos.

--- SGQ.Infra.Data/Context/SgqContext.cs
+++ SGQ.Infra.Data/Context/SgqContext.cs
@@ -1,15 +1,39 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using System.Configuration;
 using SGQ.Domain.Entities;
+using Microsoft.IdentityModel.Protocols;
 
 namespace SGQ.Infra.Data.Context
 {
     public class SgqContext : DbContext
-    {       
+    {
+        private readonly string connectionString;
+
+        public SgqContext(string connectionString) : base()
+        {
+            this.connectionString = connectionString;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+//              #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer(connectionString);
+            }
+        }
+
         public DbSet<Acao> Acao { get; set; }
         public DbSet<Atividade> Atividade { get; set; }
         public DbSet<Area> Area { get; set; }
         public DbSet<NaoConformidade> NaoConformidade { get; set; }
         public DbSet<Norma> Norma { get; set; }
-        public DbSet<Processo> Processo { get; set; }        
+        public DbSet<Processo> Processo { get; set; }
+
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlServer(ConfigurationManager<T>.ConnectionStrings["SQGDataBase"].ConnectionString,  );
+        //}
     }
 }
---
+++ Update_Rebase.bat
@@ -0,0 +1,20 @@
+echo off
+
+cls
+
+echo .
+
+echo ..........................................................................................
+
+echo Continue apenas depois de fazer o commit no repositorio local
+
+echo ..........................................................................................
+
+echo .
+
+pause
+
+
+git rebase -- continue
+
+pause
