8869702902378c7fcd3d8d8c5f79fc8e289a6ecb
William Pereira <william@williampereira.me>
2019-08-17 17:50:29 -0300

    Adicionar entidade Usuario

---
+++ SGQ.Domain/Entities/Usuario.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity;
+
+namespace SGQ.Domain.Entities
+{
+    public class Usuario : IdentityUser
+    {
+        public string Codigo { get; set; }
+        public string Nome { get; set; }
+        public string Cargo { get; set; }
+        public DateTime? DataUltimoAcesso { get; set; }
+    }
+}
