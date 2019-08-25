95e69fc0518efd45dfb26ffd7a8dba5d90117445
William Pereira <william@williampereira.me>
2019-08-25 16:51:27 -0300

    implementação inicial dos validators (estrutura)

---
+++ SGQ.Domain/Validators/Atividade/AtividadeAptaParaCadastroValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace SGQ.Domain.Validators.Atividade
+{
+    public class AtividadeAptaParaCadastroValidator : AbstractValidator<Entities.Atividade>
+    {
+        public AtividadeAptaParaCadastroValidator()
+        {
+            RuleFor(x => x.Nome)
+                .NotEmpty().WithMessage("Nome da atividade obrigatório");
+        }
+    }
+}
