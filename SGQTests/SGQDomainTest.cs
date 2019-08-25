ad475450357ac31bee1973b663634e145285f571
Rafael Barreto <rafaelbarretoo2008@hotmail.com>
2019-08-25 19:31:12 -0300

    BL_DEV: Ajuste teste

--- SGQTests/SGQDomainTest.cs
+++ SGQTests/SGQDomainTest.cs
@@ -3,11 +3,12 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace SGQTests
 {
     [TestClass]
-    public class UnitTest1
+    public class SgqDomainTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void GetRepository()
         {
+
         }
     }
 }
